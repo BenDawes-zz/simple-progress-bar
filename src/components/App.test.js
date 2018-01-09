06df636e2dcd34cf8cea9d8651e9b3893dcf9cf3
Ben Dawes <bdawes@palantir.com>
2018-01-09 07:16:19 +0000

    Added simple click test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+it('changes state after click', () => {
+  const app = mount(<App />)
+  const button = app.find('button')
+  let inactiveCount = app.find('.inactive').length
+  button.simulate('submit') // https://github.com/airbnb/enzyme/issues/308
+  let newInactiveCount = app.find('.inactive').length
+  expect(inactiveCount).toEqual(newInactiveCount + 1)
+})
